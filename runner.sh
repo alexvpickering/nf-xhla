510e9b51cc53300faf95b5e4b1b1daec47c6c707
Alex Pickering <alexvpickering@gmail.com>
2021-03-02 15:53:15 -0800

    fix conda env

--- runner.sh
+++ runner.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /n/scratch3/users/a/ap491/F20FTSUSAT1396_HUMooaR/Reads/nf-xhla
+mkdir logs
 DONE="200396 1308207 116395 11530251 11036018 103174 200634 200627 200622 200597 200617 200454"
 
 is_done() { echo $DONE | grep -F -q -w "$1"; }
